4dc1a8d8f6639b386b0dd97df70b27b7f6f7bd5a
lynxnb <niccolo.betto@gmail.com>
2023-01-31 19:45:51 +0100

    Add taffo annotations to working arrays

--- src/types/definitions.h
+++ src/types/definitions.h
@@ -44,41 +44,41 @@ typedef struct profiler_type_t {
 } profiler_type; // 120 bytes
 
 typedef struct field_type_t {
-  double *density0;     // 2D array
-  double *density1;     // 2D array
-  double *energy0;      // 2D array
-  double *energy1;      // 2D array
-  double *pressure;     // 2D array
-  double *viscosity;    // 2D array
-  double *soundspeed;   // 2D array
-  double *xvel0;        // 2D array
-  double *xvel1;        // 2D array
-  double *yvel0;        // 2D array
-  double *yvel1;        // 2D array
-  double *vol_flux_x;   // 2D array
-  double *mass_flux_x;  // 2D array
-  double *vol_flux_y;   // 2D array
-  double *mass_flux_y;  // 2D array
-  double *work_array1;  // 2D array | node_flux, stepbymass, volume_change, pre_vol
-  double *work_array2;  // 2D array | node_mass_post, post_vol
-  double *work_array3;  // 2D array | node_mass_pre, pre_mass
-  double *work_array4;  // 2D array | advec_vel, post_mass
-  double *work_array5;  // 2D array | mom_flux, advec_vol
-  double *work_array6;  // 2D array | pre_vol, post_ener
-  double *work_array7;  // 2D array | post_vol, ener_flux
+  __attribute__((annotate("scalar(range(   0,   1) final)"))) double *density0;     // 2D array
+  __attribute__((annotate("scalar(range(   0,   1) final)"))) double *density1;     // 2D array
+  __attribute__((annotate("scalar(range(   0,   3) final)"))) double *energy0;      // 2D array
+  __attribute__((annotate("scalar(range(   0,   3) final)"))) double *energy1;      // 2D array
+  __attribute__((annotate("scalar(range(   0,   1) final)"))) double *pressure;     // 2D array
+  __attribute__((annotate("scalar(range(   0,   1) final)"))) double *viscosity;    // 2D array
+  __attribute__((annotate("scalar(range(   0,   2) final)"))) double *soundspeed;   // 2D array
+  __attribute__((annotate("scalar(range(  -1,   1) final)"))) double *xvel0;        // 2D array
+  __attribute__((annotate("scalar(range(  -1,   1) final)"))) double *xvel1;        // 2D array
+  __attribute__((annotate("scalar(range(  -1,   1) final)"))) double *yvel0;        // 2D array
+  __attribute__((annotate("scalar(range(  -1,   1) final)"))) double *yvel1;        // 2D array
+  __attribute__((annotate("scalar(range(  -1,   1) final)"))) double *vol_flux_x;   // 2D array
+  __attribute__((annotate("scalar(range(  -1,   1) final)"))) double *mass_flux_x;  // 2D array
+  __attribute__((annotate("scalar(range(  -1,   1) final)"))) double *vol_flux_y;   // 2D array
+  __attribute__((annotate("scalar(range(  -1,   1) final)"))) double *mass_flux_y;  // 2D array
+  __attribute__((annotate("scalar(range(  -1,   1) final)"))) double *work_array1;  // 2D array | node_flux, stepbymass, volume_change, pre_vol
+  __attribute__((annotate("scalar(range(   0,   1) final)"))) double *work_array2;  // 2D array | node_mass_post, post_vol
+  __attribute__((annotate("scalar(range(   0,   1) final)"))) double *work_array3;  // 2D array | node_mass_pre, pre_mass
+  __attribute__((annotate("scalar(range(  -1,   1) final)"))) double *work_array4;  // 2D array | advec_vel, post_mass
+  __attribute__((annotate("scalar(range(   0,   1) final)"))) double *work_array5;  // 2D array | mom_flux, advec_vol
+  __attribute__((annotate("scalar(range(   0,   1) final)"))) double *work_array6;  // 2D array | pre_vol, post_ener
+  __attribute__((annotate("scalar(range(  -1,   1) final)"))) double *work_array7;  // 2D array | post_vol, ener_flux
 
-  double *cellx;     // 1D array
-  double *celly;     // 1D array
-  double *vertexx;   // 1D array
-  double *vertexy;   // 1D array
-  double *celldx;    // 1D array
-  double *celldy;    // 1D array
-  double *vertexdx;  // 1D array
-  double *vertexdy;  // 1D array
+  __attribute__((annotate("scalar(range(  -1,  11) final)"))) double *cellx;     // 1D array
+  __attribute__((annotate("scalar(range(  -1,  11) final)"))) double *celly;     // 1D array
+  __attribute__((annotate("scalar(range(  -1,  11) final)"))) double *vertexx;   // 1D array
+  __attribute__((annotate("scalar(range(  -1,  11) final)"))) double *vertexy;   // 1D array
+  __attribute__((annotate("scalar(range(   0,   1) final)"))) double *celldx;    // 1D array
+  __attribute__((annotate("scalar(range(   0,   1) final)"))) double *celldy;    // 1D array
+  __attribute__((annotate("scalar(range(   0,   1) final)"))) double *vertexdx;  // 1D array
+  __attribute__((annotate("scalar(range(   0,   1) final)"))) double *vertexdy;  // 1D array
 
-  double *volume;  // 2D array
-  double *xarea;   // 2D array
-  double *yarea;   // 2D array
+  __attribute__((annotate("scalar(range(   0,   1) final)"))) double *volume;  // 2D array
+  __attribute__((annotate("scalar(range(   0,   1) final)"))) double *xarea;   // 2D array
+  __attribute__((annotate("scalar(range(   0,   1) final)"))) double *yarea;   // 2D array
 } field_type; // 264 bytes
 
 typedef struct tile_type_t {
